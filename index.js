8e27dd4f3c93f00bc1aeea1bb969b9cfc28f382e
Scott Weinstein <sweinstein27@gmail.com>
2018-04-03 18:42:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Scott"
-var height = "72"
+var height = 72
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height)+1)
